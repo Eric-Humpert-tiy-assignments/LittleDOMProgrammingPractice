d7b9fe5cdeb9f4f9c57da60a772e018093c822e4
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-04-12 13:45:34 -0500

    created a directory for the HTML, CSS, and js files

---
+++ public/script.js
@@ -0,0 +1,19 @@
+console.log("here!");
+
+var button = document.querySelector('#addButton');
+var input = document.querySelector('#letterBox');
+var letters = document.querySelector('#letterList');
+
+var addLetter = function wordBuilder() {
+  var listItem = document.createElement('li');
+
+  listItem.textContent = input.value;
+
+  letters.appendChild(listItem);
+  input.value = '';
+
+}
+
+console.log(input.value);
+
+button.addEventListener('click', addLetter);
